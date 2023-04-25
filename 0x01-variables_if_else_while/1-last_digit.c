08a2ba38330fdc90855a854a53ecf9688e37e171
Chiomachristabel <okpetahchiomachristabel@gmail.com>
2023-04-24 21:30:15 -0700

    C VARIABLES ,IF ELSE , WHILE

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: 0
+ *
  */
+
 int main(void)
 {
 	int n;
@@ -15,12 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
-
 	if (lastdigit > 5)
 		printf("last digit of %i is %i and is greater than 5\n", n, lastdigit);
 	else if (lastdigit == 0)
 		printf("last digit of %i is %i and is 0\n", n, lastdigit);
 	else if (lastdigit < 6)
-	printf("last digit of %i is %i and is less than 6 and not 0\n", n, lastdigit);
+		printf("last digit of %i is %i and is less than 6 and not 0\n", n, lastdigit);
 	return (0);
 }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,41 +9,23 @@
 
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int c, d;
 
-	int c2;
-	int f_d2;
-	int l_d2;
-
-	while (c <= 98)
+	for (c = 0; c <= 98; c++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 = 0;
-		while (c2 <= 99)
+		for (d = c + 1; d <= 99; d++)
 		{
-			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
-
-			if (c < c2)
-			{
-				putchar(f_d);
-				putchar(l_d);
-				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-
-				if (c != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			c2++;
+			putchar((c / 10) + '0');
+			putchar((c % 10) + '0');
+			putchar(' ');
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+			if (c == 98 && d == 99)
+			continue;
+			putchar(',');
+			putchar(' ');
 		}
-		c++;
 	}
-return (0);
+	putchar('\n');
+	return (0);
 }
