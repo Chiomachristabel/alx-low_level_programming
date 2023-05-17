b4ecc1db0499eafbeaae042d0aa8102bce6735bc
Chiomachristabel <okpetahchiomachristabel@gmail.com>
2023-05-17 05:01:45 -0700

    ARGUMENTS TO MAIN (ARGC AND ARGV)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,64 +1,54 @@
-1.19 KiB
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
-int give_change(int money);
+
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
- * @argc: number of arguments.
- * @argv: amount of money given in singles.
+ * main - Find the smallest number of coins to make an amount of change
+ * @argc: Arg count
+ * @argv: Contains all arguments
  *
- * Return: 0 (sucesss) , 1 (Failure)
+ * Return: 1 on error, 0 for success
  */
 int main(int argc, char *argv[])
-{
-	int money, change;
-	money = change = 0;
-	if (argc != 2)
+{	int cents, coins = 0;
+
+	if (argc == 2)
+	{
+		cents = atoi(*(argv + 1));
+		while (cents > 0)
+		{
+			if (cents % 25 < cents)
+			{
+				cents -= 25;
+				coins++;
+			}
+			else if (cents % 10 < cents)
+			{
+				cents -= 10;
+				coins++;
+			}
+			else if (cents % 5 < cents)
+			{
+				cents -= 5;
+				coins++;
+			}
+			else if (cents % 2 < cents)
+			{
+				cents -= 2;
+				coins++;
+			}
+			else if (cents % 1 < cents)
+			{
+				cents -= 1;
+				coins++;
+			}
+		}
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	money = atoi(argv[1]);
-	change = give_change(money);
-	printf("%d\n", change);
+	printf("%d\n", coins);
 	return (0);
-}
-/**
- * give_change - find the minimum number of coins to make change
- * for an amount of money
- * @money: amount of money given in singles.
- *
- * Return: min amount of coins. (INT)
- */
-int give_change(int money)
-{
-	int one, two, five, ten, tf, sum;
-	sum = one = two = five = ten = tf = 0;
-	if (money <= 0)
-	{
-		return (0);
-	}
-	if (money >= 25)
-	{
-		tf = money / 25;
-		money -= tf * 25;
-	}
-	if (money < 25 && money >= 10)
-	{
-		ten = money / 10;
-		money -= ten * 10;
-	}
-	if (money < 10 && money >= 5)
-	{
-		five = money / 5;
-		money -= five * 5;
-	}
-	if (money < 5 && money >= 2)
-		two = money / 2;
-		money -= two * 2;
-	if (money < 2)
-		one = money;
-	sum = tf + ten + five + two + one;
-	return (sum);
 }
