70057396ef59cf5afd5ec229f2f0d72b15c17c79
Chiomachristabel <okpetahchiomachristabel@gmail.com>
2023-05-17 19:09:10 -0700

    C STATIC LIBRARY

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,5 +22,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int puts(const char *string);
+int main(void);
 
-#endif /* MAIN_H */
+#endif
